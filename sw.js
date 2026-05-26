6ec0f2b41f7bad74f74c0985e8e6491ea251ee36
Lê Minh Đức <86581679+duclmhe140328@users.noreply.github.com>
2026-05-26 19:23:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
 
+<script>
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('Service Worker đăng ký thành công!');
+        })
+        .catch(err => {
+          console.log('Service Worker đăng ký thất bại: ', err);
+        });
+    });
+  }
+</script>
